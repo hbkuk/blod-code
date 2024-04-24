b443f7066327b16f4cdfa650005e71593221347e
hbkuk <109803585+hbkuk@users.noreply.github.com>
2024-04-24 18:44:47 +0900

    bulk insert saveAll

--- jpa/bulk-insert/src/main/java/com/shop/core/issuedCoupon/domain/IssuedCoupon.java
+++ jpa/bulk-insert/src/main/java/com/shop/core/issuedCoupon/domain/IssuedCoupon.java
@@ -4,26 +4,24 @@ import com.shop.core.coupon.domain.Coupon;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class IssuedCoupon {
 
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long code;
-
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
-    @Column(name = "COUPON_CODE", columnDefinition = "BINARY(16)")
-    private UUID code;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long code;
+
+//    @Id
+//    @GeneratedValue(generator = "uuid2")
+//    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+//    @Column(name = "COUPON_CODE", columnDefinition = "BINARY(16)")
+//    private UUID code;
 
     @JoinColumn(name = "MEMBER_EMAIL")
     private String memberEmail;
